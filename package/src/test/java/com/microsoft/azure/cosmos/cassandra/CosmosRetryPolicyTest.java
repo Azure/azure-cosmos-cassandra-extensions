9ba21d077fd2223ac97b08743375529825b3feed
babatsai <48842041+babatsai@users.noreply.github.com>
2019-08-19 16:59:20 -0700

    Add CosmosFailoverAwareRRPolicy (#2)
    * Retry for WriteFailure and add salt to backoff time.
    
    Modifying the retry policy to consider WriteFailure, in the case of simultaneous writes to a same row.  In addition, adding a salt to the backoff time so that two sessions doesn't retry at the same time again.
    
    * Ms to millis
    
    * Added CosmosFailoverAwareRRPolicy
    
    * add docs
    
    * revert .toArray() change
    
    * java coding style changes
    
    * Fix ){
    
    * code -> reformat

--- package/src/test/java/com/microsoft/azure/cosmos/cassandra/CosmosRetryPolicyTest.java
+++ package/src/test/java/com/microsoft/azure/cosmos/cassandra/CosmosRetryPolicyTest.java
@@ -22,12 +22,11 @@ package com.microsoft.azure.cosmos.cassandra;
 import com.datastax.driver.core.*;
 import com.datastax.driver.core.exceptions.DriverException;
 import com.datastax.driver.core.exceptions.OverloadedException;
+import com.datastax.driver.core.policies.RetryPolicy;
 import org.testng.annotations.Test;
 
 import java.net.InetSocketAddress;
-import java.text.SimpleDateFormat;
 
-import static com.datastax.driver.core.BatchStatement.Type.UNLOGGED;
 import static com.datastax.driver.core.ConsistencyLevel.QUORUM;
 import static com.datastax.driver.core.policies.RetryPolicy.RetryDecision;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -40,7 +39,7 @@ import static org.assertj.core.api.Assertions.fail;
  *
  * <ul>
  * <li>An Apache Cassandra cluster is running and accessible through the contacts points
- * identified by {@link #CONTACT_POINTS} and {@link #PORT}.
+ * identified by #CONTACT_POINTS and #PORT.
  * </ul>
  * <p>
  * Side effects:
@@ -70,11 +69,10 @@ public class CosmosRetryPolicyTest {
 
     @Test(groups = {"integration", "checkintest"}, timeOut = TIMEOUT)
     public void canIntegrateWithCosmos() {
-
         CosmosRetryPolicy retryPolicy = new CosmosRetryPolicy(MAX_RETRY_COUNT, FIXED_BACK_OFF_TIME, GROWING_BACK_OFF_TIME);
 
         try {
-            this.connect(CONTACT_POINTS, PORT, retryPolicy);
+            this.connect(TestCommon.CONTACT_POINTS, TestCommon.PORT, retryPolicy);
 
         } catch (Exception error) {
             fail(String.format("connect failed with %s: %s", error.getClass().getCanonicalName(), error));
@@ -82,20 +80,20 @@ public class CosmosRetryPolicyTest {
 
         try {
             try {
-                this.createSchema();
+                TestCommon.createSchema(session);
 
             } catch (Exception error) {
                 fail(String.format("createSchema failed: %s", error));
             }
             try {
-                this.write(CONSISTENCY_LEVEL);
+                TestCommon.write(session, CONSISTENCY_LEVEL);
 
             } catch (Exception error) {
                 fail(String.format("write failed: %s", error));
             }
             try {
-                ResultSet rows = this.read(CONSISTENCY_LEVEL);
-                this.display(rows);
+                ResultSet rows = TestCommon.read(session, CONSISTENCY_LEVEL);
+                TestCommon.display(rows);
 
             } catch (Exception error) {
                 fail(String.format("read failed: %s", error));
@@ -124,42 +122,8 @@ public class CosmosRetryPolicyTest {
     public void willRethrowOverloadedExceptionWithGrowingBackOffTime() {
 
         CosmosRetryPolicy retryPolicy = new CosmosRetryPolicy(MAX_RETRY_COUNT);
-        retry(retryPolicy,MAX_RETRY_COUNT + 1, MAX_RETRY_COUNT + 1, RetryDecision.Type.RETHROW);
+        retry(retryPolicy, MAX_RETRY_COUNT + 1, MAX_RETRY_COUNT + 1, RetryDecision.Type.RETHROW);
     }
-
-    // Privates
-
-    private static final String[] CONTACT_POINTS;
-    static {
-
-        String value = System.getProperty("COSMOS_HOSTNAME");
-
-        if (value == null) {
-            value = System.getenv("COSMOS_HOSTNAME");
-        }
-
-        if (value == null) {
-            value = "localhost";
-        }
-
-        CONTACT_POINTS = new String[] {value};
-    }
-
-    private static final int PORT;
-    static {
-
-        String value = System.getProperty("COSMOS_PORT");
-
-        if (value == null) {
-            value = System.getenv("COSMOS_PORT");
-        }
-
-        if (value == null) {
-            value = "10350";
-        }
-
-        PORT = Short.parseShort(value);
-    };
 
     private static final ConsistencyLevel CONSISTENCY_LEVEL = QUORUM;
     private static final int FIXED_BACK_OFF_TIME = 5000;
@@ -169,6 +133,29 @@ public class CosmosRetryPolicyTest {
 
     private Cluster cluster;
     private Session session;
+
+    /**
+     * Tests a retry operation
+     */
+    private void retry(CosmosRetryPolicy retryPolicy, int retryNumberBegin, int retryNumberEnd, RetryPolicy.RetryDecision.Type expectedRetryDecisionType) {
+
+        DriverException driverException = new OverloadedException(new InetSocketAddress(TestCommon.CONTACT_POINTS[0], TestCommon.PORT), "retry");
+        Statement statement = new SimpleStatement("SELECT * FROM retry");
+        ConsistencyLevel consistencyLevel = CONSISTENCY_LEVEL;
+
+        for (int retryNumber = retryNumberBegin; retryNumber < retryNumberEnd; retryNumber++) {
+
+            long expectedDuration = 1000000 * (retryPolicy.getMaxRetryCount() == -1 ? FIXED_BACK_OFF_TIME : retryNumber * GROWING_BACK_OFF_TIME);
+            long startTime = System.nanoTime();
+
+            RetryDecision retryDecision = retryPolicy.onRequestError(statement, consistencyLevel, driverException, retryNumber);
+
+            long duration = System.nanoTime() - startTime;
+
+            assertThat(retryDecision.getType()).isEqualTo(expectedRetryDecisionType);
+            assertThat((double) duration).isGreaterThan(expectedDuration - 0.01 * expectedDuration);
+        }
+    }
 
     /**
      * Initiates a connection to the cluster specified by the given contact points and port.
@@ -184,156 +171,6 @@ public class CosmosRetryPolicyTest {
     }
 
     /**
-     * Creates the schema (keyspace) and table to verify that we can integrate with Cosmos.
-     */
-    private void createSchema() {
-
-        session.execute(
-                "CREATE KEYSPACE IF NOT EXISTS downgrading WITH replication "
-                        + "= {'class':'SimpleStrategy', 'replication_factor':3}");
-
-        session.execute(
-                "CREATE TABLE IF NOT EXISTS downgrading.sensor_data ("
-                        + "sensor_id uuid,"
-                        + "date date,"
-                        + // emulates bucketing by day
-                        "timestamp timestamp,"
-                        + "value double,"
-                        + "PRIMARY KEY ((sensor_id,date),timestamp)"
-                        + ")");
-    }
-
-    /**
-     * Tests a retry operation
-     */
-    private void retry(CosmosRetryPolicy retryPolicy, int retryNumberBegin, int retryNumberEnd, RetryDecision.Type expectedRetryDecisionType) {
-
-        DriverException driverException = new OverloadedException(new InetSocketAddress(CONTACT_POINTS[0], PORT), "retry");
-        Statement statement = new SimpleStatement("SELECT * FROM retry");
-        ConsistencyLevel consistencyLevel = CONSISTENCY_LEVEL;
-
-        for (int retryNumber = retryNumberBegin; retryNumber < retryNumberEnd; retryNumber++) {
-
-            long expectedDuration = 1000000 * (retryPolicy.getMaxRetryCount() == -1 ? FIXED_BACK_OFF_TIME : retryNumber * GROWING_BACK_OFF_TIME);
-            long startTime = System.nanoTime();
-
-            RetryDecision retryDecision = retryPolicy.onRequestError(statement, consistencyLevel, driverException, retryNumber);
-
-            long duration = System.nanoTime() - startTime;
-
-            assertThat(retryDecision.getType()).isEqualTo(expectedRetryDecisionType);
-            assertThat((double)duration).isGreaterThan(expectedDuration - 0.01 * expectedDuration);
-        }
-    }
-
-    /**
-     * Inserts data, retrying if necessary with a downgraded CL.
-     *
-     * @param consistencyLevel the consistency level to apply.
-     */
-    private void write(ConsistencyLevel consistencyLevel) {
-
-        System.out.printf("Writing at %s%n", consistencyLevel);
-
-        BatchStatement batch = new BatchStatement(UNLOGGED);
-
-        batch.add(
-                new SimpleStatement(
-                        "INSERT INTO downgrading.sensor_data "
-                                + "(sensor_id, date, timestamp, value) "
-                                + "VALUES ("
-                                + "756716f7-2e54-4715-9f00-91dcbea6cf50,"
-                                + "'2018-02-26',"
-                                + "'2018-02-26T13:53:46.345+01:00',"
-                                + "2.34)"));
-
-        batch.add(
-                new SimpleStatement(
-                        "INSERT INTO downgrading.sensor_data "
-                                + "(sensor_id, date, timestamp, value) "
-                                + "VALUES ("
-                                + "756716f7-2e54-4715-9f00-91dcbea6cf50,"
-                                + "'2018-02-26',"
-                                + "'2018-02-26T13:54:27.488+01:00',"
-                                + "2.47)"));
-
-        batch.add(
-                new SimpleStatement(
-                        "INSERT INTO downgrading.sensor_data "
-                                + "(sensor_id, date, timestamp, value) "
-                                + "VALUES ("
-                                + "756716f7-2e54-4715-9f00-91dcbea6cf50,"
-                                + "'2018-02-26',"
-                                + "'2018-02-26T13:56:33.739+01:00',"
-                                + "2.52)"));
-
-        batch.setConsistencyLevel(consistencyLevel);
-
-        session.execute(batch);
-        System.out.println("Write succeeded at " + consistencyLevel);
-    }
-
-    /**
-     * Queries data, retrying if necessary with a downgraded CL.
-     *
-     * @param consistencyLevel the consistency level to apply.
-     */
-    private ResultSet read(ConsistencyLevel consistencyLevel) {
-
-        System.out.printf("Reading at %s%n", consistencyLevel);
-
-        Statement statement =
-                new SimpleStatement(
-                        "SELECT sensor_id, date, timestamp, value "
-                                + "FROM downgrading.sensor_data "
-                                + "WHERE "
-                                + "sensor_id = 756716f7-2e54-4715-9f00-91dcbea6cf50 AND "
-                                + "date = '2018-02-26' AND "
-                                + "timestamp > '2018-02-26+01:00'")
-                        .setConsistencyLevel(consistencyLevel);
-
-        ResultSet rows = session.execute(statement);
-        System.out.println("Read succeeded at " + consistencyLevel);
-        return rows;
-    }
-
-    /**
-     * Displays the results on the console.
-     *
-     * @param rows the results to display.
-     */
-    private void display(ResultSet rows) {
-
-        final int width1 = 38;
-        final int width2 = 12;
-        final int width3 = 30;
-        final int width4 = 21;
-
-        String format =
-                "%-" + width1 + "s" + "%-" + width2 + "s" + "%-" + width3 + "s" + "%-" + width4 + "s"
-                        + "%n";
-
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
-
-        // headings
-        System.out.printf(format, "sensor_id", "date", "timestamp", "value");
-
-        // separators
-        drawLine(width1, width2, width3, width4);
-
-        // data
-        for (Row row : rows) {
-
-            System.out.printf(
-                    format,
-                    row.getUUID("sensor_id"),
-                    row.getDate("date"),
-                    sdf.format(row.getTimestamp("timestamp")),
-                    row.getDouble("value"));
-        }
-    }
-
-    /**
      * Closes the session and the cluster.
      */
     private void close() {
@@ -341,20 +178,5 @@ public class CosmosRetryPolicyTest {
             session.close();
             cluster.close();
         }
-    }
-
-    /**
-     * Draws a line to isolate headings from rows.
-     *
-     * @param widths the column widths.
-     */
-    private static void drawLine(int... widths) {
-        for (int width : widths) {
-            for (int i = 1; i < width; i++) {
-                System.out.print('-');
-            }
-            System.out.print('+');
-        }
-        System.out.println();
     }
 }
