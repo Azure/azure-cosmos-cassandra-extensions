6a3aa31fca4cc7b26df748f1b8611c2e1ee6cb0b
David Noble <David.Noble@microsoft.com>
2020-10-28 09:48:47 -0700

    Added configuration options for CosmosLoadBalancingPolicy (see especially CosmosLoadBalancingPolicy.Options)

--- package/src/main/java/com/azure/cosmos/cassandra/CosmosLoadBalancingPolicy.java
+++ package/src/main/java/com/azure/cosmos/cassandra/CosmosLoadBalancingPolicy.java
@@ -3,6 +3,10 @@
 
 package com.azure.cosmos.cassandra;
 
+import com.datastax.oss.driver.api.core.config.DefaultDriverOption;
+import com.datastax.oss.driver.api.core.config.DriverExecutionProfile;
+import com.datastax.oss.driver.api.core.config.DriverOption;
+import com.datastax.oss.driver.api.core.context.DriverContext;
 import com.datastax.oss.driver.api.core.cql.BatchStatement;
 import com.datastax.oss.driver.api.core.cql.BoundStatement;
 import com.datastax.oss.driver.api.core.cql.SimpleStatement;
@@ -34,16 +38,16 @@ import java.util.concurrent.atomic.AtomicInteger;
  * to route read and write requests to their corresponding data centers.
  * <p>
  * If {@link #readDC} is specified, we prioritize {@linkplain Node nodes} in the {@link #readDC} for read requests.
- * Either one of {@link #writeDC} or {@link #globalContactPoint} needs to be specified in order to determine the data
+ * Either one of {@link #writeDC} or {@link #globalEndpoint} needs to be specified in order to determine the data
  * center for write requests. If {@link #writeDC} is specified, writes will be prioritized for that region. When a
- * {@link #globalContactPoint} is specified, the write requests will be prioritized for the default write region. {@link
- * #globalContactPoint} allows the client to gracefully failover by changing the default write region addresses. {@link
- * #dnsExpirationInSeconds} is essentially the max duration to recover from the failover. By default, it is 60 seconds.
+ * {@link #globalEndpoint} is specified, the write requests will be prioritized for the default write region. {@link
+ * #globalEndpoint} allows the client to gracefully failover by changing the default write region addresses. {@link
+ * #dnsExpiryTimeInSeconds} is essentially the max duration to recover from the failover. By default, it is 60 seconds.
  */
 public final class CosmosLoadBalancingPolicy implements LoadBalancingPolicy {
 
-    private final int dnsExpirationInSeconds;
-    private final String globalContactPoint;
+    private final int dnsExpiryTimeInSeconds;
+    private final String globalEndpoint;
     private final AtomicInteger index = new AtomicInteger();
     private final String readDC;
     private final String writeDC;
@@ -53,22 +57,36 @@ public final class CosmosLoadBalancingPolicy implements LoadBalancingPolicy {
     private CopyOnWriteArrayList<Node> remoteDcNodes;
     private CopyOnWriteArrayList<Node> writeLocalDcNodes;
 
+    public CosmosLoadBalancingPolicy(DriverContext driverContext, String profileName) {
+
+        final DriverExecutionProfile profile = driverContext.getConfig().getProfile(profileName);
+
+        this.dnsExpiryTimeInSeconds = profile.getInt(Option.DNS_EXPIRY_TIME,
+            Option.DNS_EXPIRY_TIME.getDefaultValue());
+
+        this.globalEndpoint = profile.getString(Option.GLOBAL_ENDPOINT,
+            Option.GLOBAL_ENDPOINT.getDefaultValue());
+
+        this.readDC = profile.getString(Option.READ_DC,
+            Option.READ_DC.getDefaultValue());
+
+        this.writeDC = profile.getString(Option.WRITE_DC,
+            Option.WRITE_DC.getDefaultValue());
+    }
+
     private CosmosLoadBalancingPolicy(
         String readDC,
         String writeDC,
         String globalContactPoint,
-        int dnsExpirationInSeconds) {
+        int dnsExpiryTimeInSeconds) {
 
         this.readDC = readDC;
         this.writeDC = writeDC;
-        this.globalContactPoint = globalContactPoint;
-        this.dnsExpirationInSeconds = dnsExpirationInSeconds;
+        this.globalEndpoint = globalContactPoint;
+        this.dnsExpiryTimeInSeconds = dnsExpiryTimeInSeconds;
     }
 
-    public static Builder builder() {
-        return new Builder();
-    }
-
+    @Override
     public void close() {
         // nothing to do
     }
@@ -81,7 +99,7 @@ public final class CosmosLoadBalancingPolicy implements LoadBalancingPolicy {
 
         List<InetAddress> dnsLookupAddresses = new ArrayList<>();
 
-        if (!this.globalContactPoint.isEmpty()) {
+        if (!this.globalEndpoint.isEmpty()) {
             dnsLookupAddresses = Arrays.asList(this.getLocalAddresses());
         }
 
@@ -147,16 +165,6 @@ public final class CosmosLoadBalancingPolicy implements LoadBalancingPolicy {
         return queryPlan;
     }
 
-    private static void addTo(ArrayDeque<Node> queryPlan, int start, List<Node> nodes) {
-
-        final int length = nodes.size();
-        final int end = start + length;
-
-        for (int i = start; i < end; i++) {
-            queryPlan.add(nodes.get(i % length));
-        }
-    }
-
     @Override
     public void onAdd(@NonNull Node node) {
         this.onUp(node);
@@ -192,7 +200,7 @@ public final class CosmosLoadBalancingPolicy implements LoadBalancingPolicy {
     @Override
     public void onUp(@NonNull Node node) {
 
-        if (node == null || node.getDatacenter() == null) {
+        if (node.getDatacenter() == null) {
             return;
         }
 
@@ -211,6 +219,14 @@ public final class CosmosLoadBalancingPolicy implements LoadBalancingPolicy {
         }
     }
 
+    // endregion
+
+    // region Privates
+
+    static Builder builder() {
+        return new Builder();
+    }
+
     /**
      * DNS lookup based on the globalContactPoint and update localAddresses.
      *
@@ -220,7 +236,7 @@ public final class CosmosLoadBalancingPolicy implements LoadBalancingPolicy {
 
         if (this.localAddresses == null || this.dnsExpired()) {
             try {
-                this.localAddresses = InetAddress.getAllByName(this.globalContactPoint);
+                this.localAddresses = InetAddress.getAllByName(this.globalEndpoint);
                 this.lastDnsLookupTime = System.currentTimeMillis() / 1000;
             } catch (UnknownHostException error) {
                 // DNS entry may be temporarily unavailable
@@ -234,18 +250,23 @@ public final class CosmosLoadBalancingPolicy implements LoadBalancingPolicy {
         return this.localAddresses;
     }
 
+    private static void addTo(ArrayDeque<Node> queryPlan, int start, List<Node> nodes) {
+
+        final int length = nodes.size();
+        final int end = start + length;
+
+        for (int i = start; i < end; i++) {
+            queryPlan.add(nodes.get(i % length));
+        }
+    }
+
     private static CosmosLoadBalancingPolicy buildFrom(Builder builder) {
         return new CosmosLoadBalancingPolicy(builder.readDC, builder.writeDC, builder.globalEndpoint,
             builder.dnsExpirationInSeconds);
     }
 
-    @SuppressWarnings("unchecked")
-    private static List<Node> cloneList(CopyOnWriteArrayList<Node> list) {
-        return (CopyOnWriteArrayList<Node>) list.clone();
-    }
-
     private boolean dnsExpired() {
-        return System.currentTimeMillis() / 1000 > this.lastDnsLookupTime + this.dnsExpirationInSeconds;
+        return System.currentTimeMillis() / 1000 > this.lastDnsLookupTime + this.dnsExpiryTimeInSeconds;
     }
 
     private InetAddress getAddress(Node node) {
@@ -284,7 +305,7 @@ public final class CosmosLoadBalancingPolicy implements LoadBalancingPolicy {
 
     private void refreshHostsIfDnsExpired() {
 
-        if (this.globalContactPoint.isEmpty() || (this.writeLocalDcNodes != null && !this.dnsExpired())) {
+        if (this.globalEndpoint.isEmpty() || (this.writeLocalDcNodes != null && !this.dnsExpired())) {
             return;
         }
 
@@ -332,7 +353,11 @@ public final class CosmosLoadBalancingPolicy implements LoadBalancingPolicy {
         }
     }
 
-    public static class Builder {
+    // endregion
+
+    // region Types
+
+    static class Builder {
 
         private int dnsExpirationInSeconds = 60;
         private String globalEndpoint = "";
@@ -364,4 +389,33 @@ public final class CosmosLoadBalancingPolicy implements LoadBalancingPolicy {
             return this;
         }
     }
+
+    private enum Option implements DriverOption {
+
+        DNS_EXPIRY_TIME(DefaultDriverOption.LOAD_BALANCING_POLICY + "dns-expiry-time",60 ),
+        GLOBAL_ENDPOINT(DefaultDriverOption.LOAD_BALANCING_POLICY + ".global-endpoint", ""),
+        READ_DC(DefaultDriverOption.LOAD_BALANCING_POLICY + ".read-dc", ""),
+        WRITE_DC(DefaultDriverOption.LOAD_BALANCING_POLICY + ".write-dc", "");
+
+        private final Object defaultValue;
+        private final String path;
+
+        Option(String path, Object defaultValue) {
+            this.path = path;
+            this.defaultValue = defaultValue;
+        }
+
+        @SuppressWarnings("unchecked")
+        public <T> T getDefaultValue() {
+            return (T) this.defaultValue;
+        }
+
+        @NonNull
+        @Override
+        public String getPath() {
+            return this.path;
+        }
+    }
+
+    // endregion
 }
