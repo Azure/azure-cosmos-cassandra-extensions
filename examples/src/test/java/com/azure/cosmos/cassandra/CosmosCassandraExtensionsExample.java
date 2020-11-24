f3abd9b35509d90c3543dce95e69a247e5664522
David Noble <David.Noble@microsoft.com>
2020-11-23 18:55:32 -0800

    Documentation update

--- examples/src/test/java/com/azure/cosmos/cassandra/CosmosCassandraExtensionsExample.java
+++ examples/src/test/java/com/azure/cosmos/cassandra/CosmosCassandraExtensionsExample.java
@@ -10,7 +10,6 @@ import com.datastax.oss.driver.api.core.cql.BatchType;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
 import com.datastax.oss.driver.api.core.cql.Row;
 import com.datastax.oss.driver.api.core.cql.SimpleStatement;
-import com.datastax.oss.driver.api.core.cql.Statement;
 import com.datastax.oss.driver.api.core.session.Session;
 import org.testng.annotations.Test;
 
@@ -22,37 +21,70 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.testng.AssertJUnit.fail;
 
 /**
- * This example illustrates use of the Cosmos extensions for the Datastax Java Driver for Apache Cassandra® 4.X.
+ * This example illustrates use of the Cosmos extensions to Datastax Java Driver 4 for Apache Cassandra®.
  * <p>
- * TODO (DANOBLE) describe configuration entries for CosmosLoadBalancingPolicy and CosmosRetryPolicy as defined in
- *   package/src/main/resources/reference.conf and specified in examples/src/test/resources/application.conf.
- * <p>
+ * Best practices for connecting to a Cosmos DB Cassandra API instance are also encapsulated in configuration. See the
+ * settings in <a href="file:///../../../../../../resources/application.conf">{@code application.conf}</a> and
+ * <a href="file:///../../../../../../../../package/src/main/resources/reference.conf">{@code reference.conf}</a>.
+ * <h3>
  * Preconditions:
- * <ul>
- * <li>An Apache Cassandra cluster is running and accessible through the configuration specified in application.conf.
- * </ul>
- * <p>
+ * <ol>
+ * <li>A Cosmos DB Cassandra API account is required.1
+ * <li>These system variables or--alternatively--environment variables must be set.
+ * <table>
+ * <thead>
+ * <tr>
+ * <th>System variable</th>
+ * <th>Environment variable</th>
+ * <th>Description</th></tr>
+ * </thead>
+ * <tbody>
+ * <tr>
+ * <td>datastax-java-driver.basic.load-balancing-policy.global-endpoint</td>
+ * <td>AZURE_COSMOS_CASSANDRA_GLOBAL_ENDPOINT</td>
+ * <td>Global endpoint address (e.g., "database-account.cassandra.cosmos.azure.com:10350")</td></tr>
+ * <tr>
+ * <td>azure.cosmos.cassandra.read-datacenter</td>
+ * <td>AZURE_COSMOS_CASSANDRA_READ_DATACENTER</td>
+ * <td>Read datacenter name (e.g., "East US")</td></tr>
+ * <tr>
+ * <td>azure.cosmos.cassandra.write-datacenter</td>
+ * <td>AZURE_COSMOS_CASSANDRA_WRITE_DATACENTER</td>
+ * <td>Write datacenter name (e.g., "West US")</td></tr>
+ * <tr>
+ * <td>datastax-java-driver.advanced.auth-provider.username</td>
+ * <td>AZURE_COSMOS_CASSANDRA_USERNAME</td>
+ * <td>Username for authentication</td></tr>
+ * <tr>
+ * <td>datastax-java-driver.advanced.auth-provider.password</td>
+ * <td>AZURE_COSMOS_CASSANDRA_PASSWORD</td>
+ * <td>Password for authentication</td></tr>
+ * </tbody>
+ * </table>
+ * </ol>
+ * <h3>
  * Side effects:
  * <ol>
- * <li>Creates a new keyspace {@code downgrading} in the cluster, with replication factor 3. If a keyspace with this
- * name already exists, it will be reused;
- * <li>Creates a new table {@code downgrading.sensor_data}. If a table with that name exists already, it will be reused;
- * <li>Inserts a few rows, downgrading the consistency level if the operation fails;
- * <li>Queries the table, downgrading the consistency level if the operation fails;
- * <li>Displays the results on the console.
- * </ol>
- * <p>
+ * <li>Creates a keyspace in the cluster with replication factor 3. To prevent collisions especially during CI test
+ * runs, we generate a keyspace name of the form <b>downgrading_</b><i></i><random-uuid></i>. Should a keyspace by this
+ * name already exist, it is reused.
+ * <li>Creates a table within the keyspace created or reused. If a table with the given name already exists, it is
+ * reused.
+ * </li>The keyspace created or reused is then dropped. This prevents keyspaces from accumulating with repeated test
+ * runs.</ol>
+ * <h3>
  * Notes:
  * <ul>
- * <li>The downgrading logic here is similar to what {@code DowngradingConsistencyRetryPolicy} does; feel free to adapt
- * it to your application needs;
  * <li>You should never attempt to retry a non-idempotent write. See the driver's manual page on idempotence for more
- * information.
- * </ul>
+ * information.</ul>
  *
  * @see <a href="https://docs.datastax.com/en/developer/java-driver/4.9/manual/">DataStax Java Driver manual</a>
+ * @see <a href="https://docs.datastax.com/en/developer/java-driver/4.9/manual/core/configuration/">DataStax Java Driver
+ * configuration</a>
  * @see CosmosLoadBalancingPolicy
  * @see CosmosRetryPolicy
+ * @see <a href="file:///../../../../../../resources/application.conf">application.conf</a>
+ * @see <a href="file:///../../../../../../../../../package/src/main/resources/reference.conf">reference.conf</a>
  */
 public class CosmosCassandraExtensionsExample implements AutoCloseable {
 
@@ -181,7 +213,7 @@ public class CosmosCassandraExtensionsExample implements AutoCloseable {
 
         System.out.printf("Reading at %s%n", consistencyLevel);
 
-        final Statement statement = SimpleStatement.newInstance(
+        final SimpleStatement statement = SimpleStatement.newInstance(
             "SELECT sensor_id, date, timestamp, value "
                 + "FROM downgrading.sensor_data "
                 + "WHERE "
