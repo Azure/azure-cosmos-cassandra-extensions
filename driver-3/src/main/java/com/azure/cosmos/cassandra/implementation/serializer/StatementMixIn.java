77d0093f68fdbd93b8905096508c79258df785a7
David Noble <david-noble@Danoble-MBP-6.thenobles.us>
2021-07-03 23:07:06 -0700

    Json bug fix: serializers are now enumerated from jars or the file system.

--- driver-3/src/main/java/com/azure/cosmos/cassandra/implementation/serializer/StatementMixIn.java
+++ driver-3/src/main/java/com/azure/cosmos/cassandra/implementation/serializer/StatementMixIn.java
@@ -6,17 +6,18 @@ package com.azure.cosmos.cassandra.implementation.serializer;
 import com.datastax.driver.core.Statement;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.annotation.JsonAppend;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 /**
  * A mix-in for serializing a {@link Statement} object into JSON.
  */
 @JsonAppend()
-@JsonInclude(value = Include.NON_NULL)
 @JsonPropertyOrder(value = { "queryString", "tracing", "fetchSize", "readTimeoutMillis" }, alphabetic = true)
-@JsonIgnoreProperties({ "namedValues", "nowInSeconds", "outgoingPayload", "routingKey", "valueNames", "values" })
+@JsonIgnoreProperties({ "nowInSeconds", "outgoingPayload", "routingKey", "valueNames" })
+@JsonInclude(NON_NULL)
 public abstract class StatementMixIn {
     public static final Class<Statement> HANDLED_TYPE = Statement.class;
 }
